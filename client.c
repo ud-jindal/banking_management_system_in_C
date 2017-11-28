3d1693fb195e92e74ae3e103946fdeeb182bcd01
ud-jindal <jindaludit23@gmail.com>
2017-11-29 04:01:23 +0530

    basic flow working

--- client.c
+++ client.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>   //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>   //close
+#include <arpa/inet.h>    //close
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
 
 void error(char* msg) {
   perror(msg);
@@ -16,7 +21,7 @@ int main(int argc, char* argv[]) {
 
   char buffer[256];
 
-  port_no = atoi(argv[1]);
+  port_no = 8887;
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -28,23 +33,27 @@ int main(int argc, char* argv[]) {
   server_add.sin_addr.s_addr = inet_addr("127.0.0.1");
   server_add.sin_port = htons(port_no);
 
-  if(connect(sockfd, &server_add, sizeof(server_add)) < 0) {
+  if(connect(sockfd, (struct sockaddr *) &server_add, sizeof(server_add)) < 0) {
     error("ERROR on connect");
   }
 
   printf("Welcome to online banking management system\n Please enter your login type\n0: admin\n1: normal\n2: joint\n");
   int login;
-  scanf("%d\n", &login);
-  write(sockfd, buffer, 256);
+  scanf("%d", &login);
+  write(sockfd, &login, sizeof(login));
   // 0: Admin, 1: normal, 2: joint
   if(login == 0) {
-
+    int operation;
+    scanf("%d", &operation);
+    write(sockfd, &operation, sizeof(operation));
   }
   else if(login == 1 || login == 2) {
-
+    int operation;
+    scanf("%d", &operation);
+    write(sockfd, &operation, sizeof(operation));
   }
   else {
-    
+
   }
 
 
--- server.c
+++ server.c
@@ -1,39 +1,53 @@
-  #include <stdio.h>
-#include <stdint.h>
-#include <sys/resource.h>
+#include <stdio.h>
+#include <string.h>   //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>   //close
+#include <arpa/inet.h>    //close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+
+void error(char* msg) {
+  perror(msg);
+  exit(1);
+}
 
 int main () {
   int sockfd, new_sock_fd, port_no, clilen;
   char buffer[256];
   struct sockaddr_in server_add, client_add;
-  while(true) {
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if(sockfd < 0) {
-      error("ERROR on opening socket");
-    }
+  if(sockfd < 0) {
+    error("ERROR on opening socket");
+  }
 
-    bzero(&server_add, sizeof(server_add));
+  bzero(&server_add, sizeof(server_add));
 
-    port_no = atoi(argv[1]);
+  port_no = 8887;
 
-    server_add.sin_family = AF_INET;
-    server_add.sin_addr.s_addr = INADDR_ANY;
-    server_add.sin_port = htons(port_no);
+  server_add.sin_family = AF_INET;
+  server_add.sin_addr.s_addr = INADDR_ANY;
+  server_add.sin_port = htons(port_no);
 
-    if (bind(sockfd, (struct sockaddr *) &server_add, sizeof(server_add)) < 0) {
-      error("ERROR on binding");
-    }
-
-    listen(sockfd, 5);
+  if (bind(sockfd, (struct sockaddr *) &server_add, sizeof(server_add)) < 0) {
+    error("ERROR on binding");
+  }
+  while(1) {
+    listen(sockfd, 10);
     int addrlen = sizeof(client_add);
     new_sock_fd = accept(sockfd, (struct sockaddr *) &client_add, (socklen_t*)&addrlen);
+    printf("---------------\n" );
     if(fork() == 0) {
+      printf("In child process\n");
       int login; // 0: Admin, 1: normal, 2: joint
-      read(new_sock_fd, login, sizeof(login));
+      read(new_sock_fd, &login, sizeof(login));
+      printf("%d\n", login);
       if(login == 0) {
-        int operation // 0: add, 1: delete, 2: modify, 3: search;
-        read(new_sock_fd, login, sizeof(login));
+        int operation; // 0: add, 1: delete, 2: modify, 3: search;
+        read(new_sock_fd, &login, sizeof(login));
         if(operation == 0) {
 
         }
@@ -52,7 +66,7 @@ int main () {
       }
       else if(login == 1 || login == 2) {
         int operation;
-        read(new_sock_fd, login, sizeof(login));
+        read(new_sock_fd, &login, sizeof(login));
         // Deposit
         if(operation == 0) {
 
